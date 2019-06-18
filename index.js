b304fdb7738003b0698b54b7491894e52a05695f
Jade Kneen <jadekneen@hotmail.com>
2019-06-18 15:34:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+// function (start, range) {
+//     let getRandom = Math.floor((Math.random()*range)+start);
+//     while(getRandom > range){
+//         getRandom = Math.floor((Math.random()*range)+start);
+//     }
+//     return getRandom;
+// }
+
+// console.log(getRandomNumber(1, 92))
